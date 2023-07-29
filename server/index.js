8d44a0df16ce41478f326c464c322bb96d9d80c5
Nitin Gupta <nitingupta6862@gmail.com>
2023-07-29 14:25:40 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -14,9 +14,22 @@ const fs=require('fs');
 
 
 const salt = bcrypt.genSaltSync(10);
-const secret = 'vjajgguvhjbajguiv';
+const secret = process.env.SECRET;
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+const allowedDomains = ['https://animated-fenglisu-78b3ae.netlify.app', 'http://localhost:3000'];
+app.use(cors({ credentials:true,
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+ 
+    if (allowedDomains.indexOf(origin) === -1) {
+      const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads' , express.static(__dirname + '/uploads'));
